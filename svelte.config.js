a52558dba312dd080ce471ff80bb15ac5e017fd2
kashish.sahu <125540624+gitKashish@users.noreply.github.com>
2026-03-27 13:22:03 +0530

    PATCH: Prender options updated

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,13 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: '404.html',
+			precompress: false,
+			strict: true
+		})
 	},
 	preprocess: [mdsvex(), mdsvex({ extensions: ['.svx', '.md'] })],
 	extensions: ['.svelte', '.svx', '.md']
